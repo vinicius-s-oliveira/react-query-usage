b248664404d8430b6d30087480c3c5abe5b4030c
vinicius.s.oliveira <vinicius.s.oliveira@accenture.com>
2024-08-29 18:15:08 -0300

    feat: inserts list from mockapi using axios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,99 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, Image } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Animated from "react-native-reanimated";
+
+export interface UserList {
+  id: number;
+  name: string;
+  avatar: string;
+}
 
 export default function HomeScreen() {
+  const [userList, setUserList] = useState<UserList[]>();
+
+  useEffect(() => {
+    axios
+      .get("https://66d0d61d181d059277dfde9c.mockapi.io/react-query-list/users")
+      .then((response) => {
+        const users = response.data as UserList[];
+        setUserList(users);
+        console.log(
+          "users:",
+          users.map((user) => user.name)
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <Animated.ScrollView>
+        <Animated.View style={styles.textContainer}>
+          <Animated.Text style={styles.text}>React Query Usage</Animated.Text>
+        </Animated.View>
+
+        {userList && (
+          <>
+            {userList.map((user) => {
+              return (
+                <Animated.View key={user.id} style={styles.userContainer}>
+                  <Animated.View
+                    style={{
+                      backgroundColor: "blue",
+                    }}
+                  >
+                    <Animated.Image
+                      source={{ uri: `${user.avatar}` }}
+                      style={styles.image}
+                    />
+                  </Animated.View>
+
+                  <Animated.Text style={styles.nameText}>
+                    {user.name}
+                  </Animated.Text>
+                </Animated.View>
+              );
+            })}
+          </>
+        )}
+      </Animated.ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  textContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 60,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  userContainer: {
+    justifyContent: "center",
+    flexDirection: "row",
+    width: "100%",
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 24,
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 24,
+    color: "#2b6cb0",
+  },
+  nameText: {
+    fontSize: 18,
+    paddingBottom: 6,
+  },
+  image: {
+    width: 80,
+    height: 80,
   },
 });
