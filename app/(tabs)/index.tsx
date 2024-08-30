0e0de65c16a82bd2ebf652563a73c596461288a0
vinicius.s.oliveira <vinicius.s.oliveira@accenture.com>
2024-08-30 11:49:56 -0300

    feat: adds flatlist refactoring and avatar images from mockapi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { StyleSheet, SafeAreaView, Image } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 import Animated from "react-native-reanimated";
 
@@ -31,36 +31,35 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Animated.ScrollView>
-        <Animated.View style={styles.textContainer}>
-          <Animated.Text style={styles.text}>React Query Usage</Animated.Text>
-        </Animated.View>
+      <Animated.View style={styles.titleContainer}>
+        <Animated.Text style={styles.title}>React Query Usage</Animated.Text>
+      </Animated.View>
 
+      <Animated.View style={styles.bodyContainer}>
         {userList && (
-          <>
-            {userList.map((user) => {
+          <Animated.FlatList
+            data={userList}
+            renderItem={({ item }) => {
               return (
-                <Animated.View key={user.id} style={styles.userContainer}>
-                  <Animated.View
-                    style={{
-                      backgroundColor: "blue",
-                    }}
-                  >
+                <Animated.View key={item.id} style={styles.userContainer}>
+                  <Animated.View>
                     <Animated.Image
-                      source={{ uri: `${user.avatar}` }}
+                      source={{ uri: `${item.avatar}` }}
                       style={styles.image}
                     />
                   </Animated.View>
 
-                  <Animated.Text style={styles.nameText}>
-                    {user.name}
-                  </Animated.Text>
+                  <Animated.View style={styles.nameContainer}>
+                    <Animated.Text style={styles.nameText}>
+                      {item.name}
+                    </Animated.Text>
+                  </Animated.View>
                 </Animated.View>
               );
-            })}
-          </>
+            }}
+          />
         )}
-      </Animated.ScrollView>
+      </Animated.View>
     </SafeAreaView>
   );
 }
@@ -69,31 +68,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  textContainer: {
+  titleContainer: {
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
     height: 60,
   },
+  bodyContainer: {
+    flex: 1,
+    width: "100%",
+  },
   userContainer: {
-    justifyContent: "center",
+    justifyContent: "flex-start",
     flexDirection: "row",
     width: "100%",
     paddingTop: 12,
     paddingBottom: 12,
     paddingLeft: 24,
   },
-  text: {
-    fontWeight: "600",
-    fontSize: 24,
+  title: {
+    fontWeight: "700",
+    textTransform: "uppercase",
+    fontSize: 22,
     color: "#2b6cb0",
   },
   nameText: {
     fontSize: 18,
     paddingBottom: 6,
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  nameContainer: {
+    justifyContent: "center",
+    marginLeft: 12,
   },
   image: {
-    width: 80,
-    height: 80,
+    width: 60,
+    height: 60,
+    borderRadius: 50,
   },
 });
